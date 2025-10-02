04767f44dac534037dbfe8dad4fff93d0215d30a
allan <centralkim254@gmail.com>
2025-10-02 14:44:05 +0300

    Add files via upload

---
+++ user credentials i,o.c
@@ -0,0 +1,15 @@
+/*Name: Allan Kimutai
+Registration No: PA106/G/28722/25
+Description: A program A program prompting the user to enter their credentials and displaying it.
+*/
+//A program prompting the user to enter their credentials.
+#include<stdio.h>
+{
+int age;
+printf("Enter your age:");
+scanf("%d,age");
+printf("You are%d years old,"age);
+
+    
+    
+}
